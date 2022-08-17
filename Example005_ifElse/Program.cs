e5bd23ceb5b109d0465d8213973f503dd3c47b39
Nik <nik@yahoo.com>
2022-08-17 20:51:33 +0700

    Example 005 If-Else

---
+++ Example005_ifElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string userName = Console.ReadLine();
+
+if (userName.ToLower() == "nik")
+{
+    Console.WriteLine("Ура, это же Nik!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(userName);
+}
